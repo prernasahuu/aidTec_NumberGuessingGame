f28fa46a3fb6a10e079e5c16278130eb61643be5
Prerna Sahu <123632781+prernasahuu@users.noreply.github.com>
2023-05-26 00:09:59 +0530

    number guessing game.py

--- number guessing.py
+++ number guessing.py
@@ -1,27 +1,32 @@
 import random
+
 def number_guessing_game():
-  n = random.randint(1, 100)
-  print("WELCOME TO THE NUMBER GUESSING GAME!")
-  name = str(input("Enter your name:"))
-  print(f"I am thinking of a number from 1 to 100. Can you guess it {name}...")
-  attempts = 0
-  while attempts < 10:
-    guess = int(input("Enter your guess: "))
-    attempts += 1
-    if guess == n:
-      print(
-        f"CONGRATULATIONS {name}!! You have guessed it Right in {attempts} attempts!"
-      )
-      print("Bravo!! You're the WINNER.")
-      break
-    elif guess < n:
-      print("Your guess is Too LOW! Try again.")
-    else:
-      print("Your guess is Too High!, Try again.")
-      if attempts == 10:
-        print(
-          f"Game Over! You have reached the maximum number of attempts. The Guessed Number was {n}.")
-        play_again= input("Do you want to play again yes/no?")
-        if play_again.lower()!="yes":
-          break
+    print("WELCOME TO THE NUMBER GUESSING GAME!")
+    name = str(input("Enter your name:"))
+    print(f"I am thinking of a number from 1 to 100. Can you   guess it {name}...")
+    
+    while True:
+        n= random.randint(1, 100)
+        attempts = 0
+        
+        while attempts < 10:
+            guess = int(input("Enter your guess: "))
+            attempts += 1
+            
+            if guess == n:
+                print(f"Congratulations! You guessed the number in {attempts} attempts!")
+                print("BRAVO!! You're the winner...")
+                break
+            elif guess < n:
+                print("Too low! Try again.")
+            else:
+                print("Too high! Try again.")
+        
+        if attempts == 10:
+            print(f"Game Over! You reached the maximum number of attempts. The secret number was {n}.")
+        
+        play_again = input("Do you want to play again? (yes/no): ")
+        if play_again.lower() != "yes":
+            break
+
 number_guessing_game()
